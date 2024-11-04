39293a73a6ecee0d36f22125c11039a885b59323
Asim Iskandarli <141365286+asim-iskandarli@users.noreply.github.com>
2024-11-04 17:42:14 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todoapp/"
+  base: "https://asim-iskandarli.github.io/TodoApp-ReactJS/"
 })
